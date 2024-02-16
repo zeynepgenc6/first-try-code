ac89fb6a3dafded632172e1453221b38eaf589cf
zeynepgenc6 <zeynepgenc6@gmail.com>
2024-02-15 21:16:30 -0600

    created the CSS and JS file

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeTheme() {
+  let body = document.querySelector("body");
+
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark");
+  } else {
+    body.classList.add("dark");
+  }
+}
+
+let themeButton = document.querySelector(".theme-button");
+themeButton.addEventListener("click", changeTheme);
